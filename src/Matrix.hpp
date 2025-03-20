bb4e5411b5f3032fc95ddfbf6af182977bf0e219
mike-landl <13545527+mike-landl@users.noreply.github.com>
2025-03-20 08:23:30 +0100

    refactor matrix class

--- src/Matrix.hpp
+++ src/Matrix.hpp
@@ -18,6 +18,7 @@ namespace FlightData
         ColumnMajor
     };
 
+    // todo: add copy constructor and assignment?
     template <typename REAL, MatrixLayout LAYOUT>
     class Matrix
     {
@@ -33,84 +34,24 @@ namespace FlightData
         { };
 
         // The initializer list is always supplied in RowMajor layout!
-        void SetMatrix(std::initializer_list<REAL> values)
-        {
-            Ensure(values.size() == elements_, "Error: Length of initializer list does not match matrix dimensions!");
-            
-            if constexpr (LAYOUT == MatrixLayout::RowMajor)
-            {
-                std::copy(values.begin(), values.end(), data_.begin());
-            }
-            else if constexpr (LAYOUT == MatrixLayout::ColumnMajor)
-            {
-                auto it = values.begin();
-                for (size_t row = 0; row < rows_; ++row)
-                for (size_t col = 0; col < cols_; ++col)
-                {
-                    (*this)(row, col) = *it++;
-                }
-            }
-        }
+        auto SetMatrix(std::initializer_list<REAL> values) -> void;
 
-        REAL& operator()(size_t row, size_t col)
-        {
-            // this if expression should have zero run time overhead
-            if constexpr (LAYOUT == MatrixLayout::ColumnMajor)
-            {
-                // for column major the "fastest changing index" is the row index
-                return data_[col * rows_ + row];
-            }
-            else if constexpr (LAYOUT == MatrixLayout::RowMajor)
-            {
-                // for row major the "fastest changing index" is the column index
-                return data_[row * cols_ + col];
-            }
-        }
-
-        const REAL& operator()(size_t row, size_t col) const
-        {
-            // this if expression should have zero run time overhead
-            if constexpr (LAYOUT == MatrixLayout::ColumnMajor)
-            {
-                // for column major the "fastest changing index" is the row index
-                return data_[col * rows_ + row];
-            }
-            else if constexpr (LAYOUT == MatrixLayout::RowMajor)
-            {
-                // for row major the "fastest changing index" is the column index
-                return data_[row * cols_ + col];
-            }
-        }
+        auto operator()(size_t row, size_t col)       ->       REAL&;
+        auto operator()(size_t row, size_t col) const -> const REAL&;
 
         // Matrix Matrix Multiplication
-        Matrix<REAL, LAYOUT> operator*(const Matrix<REAL, LAYOUT>& B) const
-        {
-            Matrix<REAL, LAYOUT> C(rows_, B.cols_);
+        auto operator*(const Matrix<REAL, LAYOUT>& B) const -> Matrix<REAL, LAYOUT>;
 
-            for (size_t i = 0; i <   rows_; ++i)
-            for (size_t j = 0; j < B.cols_; ++j)
-            {
-                REAL sum = 0;
-                for (size_t k = 0; k < cols_; ++k)
-                {
-                    sum += (*this)(i, k) * B(k, j);
-                }
-                C(i, j) = sum;
-            }
-
-            return C;
-        }
-
-        void DebugPrint(std::string label);
-        void DebugPrint() const;
+        auto DebugPrint(std::string label) -> void;
+        auto DebugPrint() const -> void;
 
         // getters for private members
-              REAL* RawPtr()       { return data_.data(); }
-        const REAL* RawPtr() const { return data_.data(); }
+        auto RawPtr()       ->       REAL* { return data_.data(); }
+        auto RawPtr() const -> const REAL* { return data_.data(); }
 
-        size_t rows()     const { return rows_; }
-        size_t cols()     const { return cols_; }
-        size_t elements() const { return elements_; }
+        auto rows()     const -> size_t { return rows_; }
+        auto cols()     const -> size_t { return cols_; }
+        auto elements() const -> size_t { return elements_; }
 
     private:
         std::vector<REAL> data_;
@@ -119,6 +60,77 @@ namespace FlightData
         size_t elements_ = 0;
     };
 
+    template <typename REAL, MatrixLayout LAYOUT>
+    void Matrix<REAL, LAYOUT>::SetMatrix(std::initializer_list<REAL> values)
+    {
+        Ensure(values.size() == elements_, "Error: Length of initializer list does not match matrix dimensions!");
+            
+        if constexpr (LAYOUT == MatrixLayout::RowMajor)
+        {
+            std::copy(values.begin(), values.end(), data_.begin());
+        }
+        else if constexpr (LAYOUT == MatrixLayout::ColumnMajor)
+        {
+            auto it = values.begin();
+            for (size_t row = 0; row < rows_; ++row)
+            for (size_t col = 0; col < cols_; ++col)
+            {
+                (*this)(row, col) = *it++;
+            }
+        }
+    }
+
+    template <typename REAL, MatrixLayout LAYOUT>
+    REAL&  Matrix<REAL, LAYOUT>::operator()(size_t row, size_t col)
+    {
+        // this if expression should have zero run time overhead
+        if constexpr (LAYOUT == MatrixLayout::ColumnMajor)
+        {
+            // for column major the "fastest changing index" is the row index
+            return data_[col * rows_ + row];
+        }
+        else if constexpr (LAYOUT == MatrixLayout::RowMajor)
+        {
+            // for row major the "fastest changing index" is the column index
+            return data_[row * cols_ + col];
+        }
+    }
+
+    template <typename REAL, MatrixLayout LAYOUT>
+    const REAL&  Matrix<REAL, LAYOUT>::operator()(size_t row, size_t col) const
+    {
+        // this if expression should have zero run time overhead
+        if constexpr (LAYOUT == MatrixLayout::ColumnMajor)
+        {
+            // for column major the "fastest changing index" is the row index
+            return data_[col * rows_ + row];
+        }
+        else if constexpr (LAYOUT == MatrixLayout::RowMajor)
+        {
+            // for row major the "fastest changing index" is the column index
+            return data_[row * cols_ + col];
+        }
+    }
+
+    // Matrix Matrix Multiplication
+    template <typename REAL, MatrixLayout LAYOUT>
+    Matrix<REAL, LAYOUT> Matrix<REAL, LAYOUT>::operator*(const Matrix<REAL, LAYOUT>& B) const
+    {
+        Matrix<REAL, LAYOUT> C(rows_, B.cols_);
+
+        for (size_t i = 0; i <   rows_; ++i)
+        for (size_t j = 0; j < B.cols_; ++j)
+        {
+            REAL sum = 0;
+            for (size_t k = 0; k < cols_; ++k)
+            {
+                sum += (*this)(i, k) * B(k, j);
+            }
+            C(i, j) = sum;
+        }
+
+        return C;
+    }
 
     template <typename REAL, MatrixLayout LAYOUT>
     void Matrix<REAL, LAYOUT>::DebugPrint(std::string label)
