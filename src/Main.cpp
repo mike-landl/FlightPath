6c08dd66aec2131cacfa6500bcf2a5c3d5bb9b01
mike-landl <13545527+mike-landl@users.noreply.github.com>
2025-03-20 10:39:08 +0100

    use logging class

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,10 +5,17 @@
 #include <catch2/catch_session.hpp> 
 
 #include "Matrix.hpp"
+#include "Log.hpp"
 
 auto main([[maybe_unused]] int argc, char* argv[]) -> int
 {
-    std::println("Hello World!");    
+    FlightData::Log::Debug("Hello World!");
+    FlightData::Log::Info("Hello World!");
+    FlightData::Log::Warn("Hello World!");
+    FlightData::Log::Error("Hello World!");
+
+    std::println("");
+    FlightData::Log::Info("Running unit test:");
     std::vector<const char*> new_argv{ argv[0], "--reporter", "console", "--durations", "yes", "--verbosity", "high", "-#"};
     int result = Catch::Session().run(static_cast<int>(new_argv.size()), const_cast<char**>(new_argv.data()));
     return result;
