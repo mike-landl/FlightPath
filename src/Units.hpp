30ab7f5d10eb37471a492cb83652453d9d1836c9
mike-landl <13545527+mike-landl@users.noreply.github.com>
2025-03-21 10:13:50 +0100

    move unit literals into units.hpp

---
+++ src/Units.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+static constexpr long double PI = 3.14159265358979323846264338327950288419716939937510l;
+
+constexpr double deg2rad(long double value)
+{
+    constexpr long double factor = PI / 180.0l;
+    return static_cast<double>(value * factor);
+}
+
+constexpr double rad2deg(long double value)
+{
+    constexpr long double factor = 180.0l / PI;
+    return static_cast<double>(value * factor);
+}
+
+constexpr double mm2m(long double value)
+{
+    constexpr long double factor = 1.0l / 1000.0l;
+    return static_cast<double>(value * factor);
+}
+
+constexpr double km2m(long double value)
+{
+    constexpr long double factor = 1000.0l;
+    return static_cast<double>(value * factor);
+}
+
+constexpr double operator"" _deg(long double value)
+{
+    return deg2rad(value);
+}
+
+constexpr double operator"" _mm(long double value)
+{
+    return static_cast<double>(mm2m(value));
+}
+
+constexpr double operator"" _m(long double value)
+{
+    return static_cast<double>(value);
+}
+
+constexpr double operator"" _km(long double value)
+{
+    return static_cast<double>(km2m(value));
+}
