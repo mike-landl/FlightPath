7930e92dc00c7ba0152ecb2161895bad7a5f40de
mike-landl <13545527+mike-landl@users.noreply.github.com>
2025-03-23 09:22:50 +0100

    add vector operations and unit tests to vec3 struct

--- src/Vec3.hpp
+++ src/Vec3.hpp
@@ -5,8 +5,14 @@ namespace FlightData
     template <typename REAL>
     struct Vec3
     {
-        REAL x, y, z;
-        Vec3() : x(0), y(0), z(0) {};
-        Vec3(REAL x, REAL y, REAL z) : x(x), y(y), z(z) {}
+        REAL x;
+        REAL y;
+        REAL z;
+
+        auto inline operator+(const Vec3 &other) -> Vec3;
+        auto inline operator-(const Vec3 &other) -> Vec3;
+        auto inline operator*(const REAL scalar) -> Vec3;
+        auto inline Dot(const Vec3 &other) -> REAL;
+        auto inline Cross(const Vec3 &other) -> Vec3;
     };
 }
