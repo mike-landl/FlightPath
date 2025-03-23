9b92f032f883259ff2fa731a4e5c57399afd715f
mike-landl <13545527+mike-landl@users.noreply.github.com>
2025-03-23 18:11:08 +0100

    add namespace to my integer types

--- src/Types.hpp
+++ src/Types.hpp
@@ -2,7 +2,10 @@
 
 #include <cstdint>
 
-using i32 =  int32_t;
-using i64 =  int64_t;
-using u32 = uint32_t;
-using u64 = uint64_t;
+namespace FlightData
+{
+    using i32 =  int32_t;
+    using i64 =  int64_t;
+    using u32 = uint32_t;
+    using u64 = uint64_t;
+}
