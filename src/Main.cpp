2b5dfe4396ab79142d1cd7ff3c48e174245ef2e2
mike-landl <13545527+mike-landl@users.noreply.github.com>
2025-03-24 12:17:21 +0100

    make FlightData a library and split tests from it, also build a flight data executable

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,33 +1,16 @@
 #include <iostream>
 #include <print>
 
-#define CATCH_CONFIG_RUNNER
-#include <catch2/catch_session.hpp> 
-
 #include "Log.hpp"
 #include "Error.hpp"
 #include "Solver.hpp"
 
-static auto RunUnitTests([[maybe_unused]]int argc, char* argv[]) -> void
-{
-    FlightData::Log::Info("Running unit test:");
-    std::vector<const char*> new_argv{ argv[0], "--reporter", "console", "--durations", "yes", "--verbosity", "high", "-#"};
-    const int result = Catch::Session().run(static_cast<int>(new_argv.size()), const_cast<char**>(new_argv.data()));
-    FlightData::Ensure(result == EXIT_SUCCESS, "Unit Tests failed.");
-}
-
-static auto RunFlightData() -> void
-{
-    FlightData::Solver solver;
-    solver.Run();
-}
-
-auto main(int argc, char* argv[]) -> int
+auto main() -> int
 {
     try
     {
-        RunUnitTests(argc, argv);
-        RunFlightData();
+        FlightData::Solver solver;
+        solver.Run();
     }
     catch (const FlightData::Exception &err)
     {
