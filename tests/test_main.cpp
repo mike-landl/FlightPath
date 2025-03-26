040e719244c3955a7eb8adee6fc9318f544c75e6
mike-landl <13545527+mike-landl@users.noreply.github.com>
2025-03-26 11:30:20 +0100

    trying to get ctest up and running

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,25 +1,2 @@
-#define CATCH_CONFIG_RUNNER
-#include <catch2/catch_session.hpp>
-
-#include <vector>
-#include <filesystem>
-
-int main([[maybe_unused]] int argc, char* argv[])
-{
-    std::filesystem::current_path(PROJECT_ROOT_PATH);
-
-    std::vector<const char*> new_argv{
-        argv[0],
-        "--reporter", "console",
-        "--durations", "yes",
-        "--verbosity", "high",
-        "-#"
-    };
-
-    const int result = Catch::Session().run(
-        static_cast<int>(new_argv.size()),
-        const_cast<char**>(new_argv.data())
-    );
-
-    return result;
-}
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch_all.hpp>
