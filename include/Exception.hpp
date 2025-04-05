fe299fe9a8e8247142da8cefed535d278f207699
mike-landl <13545527+mike-landl@users.noreply.github.com>
2025-04-05 18:12:41 +0200

    remove unused code from Exception.hpp (formatter)

--- include/Exception.hpp
+++ include/Exception.hpp
@@ -26,37 +26,3 @@ namespace FlightPath
     };
 
 }
-
-/**
- * @brief Formatter specialization for FlightPath::Exception.
- *
- * Enables the use of std::format with FlightPath::Exception, formatting
- * the exception using its what() message.
- */
-template <>
-struct std::formatter<FlightPath::Exception>
-{
-    /**
-     * @brief  Parses the format specifier (unused in this case).
-     * 
-     * @param  ctx The format parse context.
-     * @return     An iterator pointing to the end of the parsed format specifier.
-     */
-    constexpr auto parse(std::format_parse_context &ctx)
-    {
-        return std::begin(ctx);
-    }
-
-    /**
-     * @brief  Formats the FlightPath::Exception object.
-     * 
-     * @param  obj The FlightPath::Exception instance to format.
-     * @param  ctx The format context to write to.
-     * @return     An iterator to the end of the output.
-     */
-    auto format(const FlightPath::Exception &obj, std::format_context &ctx) const
-    {
-        return std::format_to(ctx.out(), "{}", obj.what());
-    }
-
-};
