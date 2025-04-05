9fca61bf45247bd89ef9cfe6ca6a467476817f86
mike-landl <13545527+mike-landl@users.noreply.github.com>
2025-04-05 06:24:44 +0200

    add exception tests

---
+++ tests/test_Exception.cpp
@@ -0,0 +1,11 @@
+#include "Exception.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_string.hpp>
+
+TEST_CASE("[Exception] Behaves like a runtime_error", "[Exception]")
+{
+    REQUIRE_THROWS_AS(throw FlightPath::Exception("Test error"), FlightPath::Exception);
+    REQUIRE_THROWS_AS(throw FlightPath::Exception("Test error"), std::runtime_error);
+    REQUIRE_THROWS_WITH(throw FlightPath::Exception("Test error"), "Test error");
+}
